955e06ea2448c155d2843a953bca78b421c60841
Mohammad Marwan Sultan <mohammad.marwan.sultan@gmail.com>
2021-01-08 18:26:26 +0200

    Installed bootstrap and added basic styling for login form

--- src/components/login_form.component.js
+++ src/components/login_form.component.js
@@ -13,45 +13,59 @@ const LoginForm = () => {
                 "email": values.email,
                 "password": values.password
             })
-            .then(res => {
-                alert(res.data);
-                console.log(res.headers["token"]);
-            })
-            .catch(err => {
-                if (err.response) {
-                    alert(err.response.data);
-                    console.log(err.response);
-                  }
-                  else if (err.request) {
-                    console.log(err.request);
-                  }
-                  else {
-                    console.log('Error', err.message);
-                  }
-            });
+                .then(res => {
+                    alert(res.data);
+                    console.log(res.headers["token"]);
+                })
+                .catch(err => {
+                    if (err.response) {
+                        alert(err.response.data);
+                        console.log(err.response);
+                    }
+                    else if (err.request) {
+                        console.log(err.request);
+                    }
+                    else {
+                        console.log(err.message);
+                    }
+                    console.log(err.toJSON());
+                });
         },
     });
-    
+
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <label htmlFor="email">Email Address</label><br/>
-            <input
-                id="email"
-                name="email"
-                type="email"
-                onChange={formik.handleChange}
-                value={formik.values.email}
-            /><br/>
-            <label htmlFor="password">Password</label><br/>
-            <input
-                id="password"
-                name="password"
-                type="password"
-                onChange={formik.handleChange}
-                value={formik.values.password}
-            /><br/>
-            <button type="submit">Submit</button>
-        </form>
+        <div className="container">
+            <div className="row">
+                <div className="card col-12 col-sm-6 offset-sm-3">
+                    <div className="card-title d-flex justify-content-center">
+                        Login
+                    </div>
+                    <div className="card-body">
+                        <form onSubmit={formik.handleSubmit}>
+                            <label htmlFor="email">Email Address</label><br />
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                onChange={formik.handleChange}
+                                value={formik.values.email}
+                            /><br />
+                            <label htmlFor="password">Password</label><br />
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                onChange={formik.handleChange}
+                                value={formik.values.password}
+                            /><br />
+                            <div className="">
+                            <   button type="submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 };
 
