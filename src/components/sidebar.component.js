88b1cab9017cc364444b0557b90c23bd6ac77112
mariamhindawi <mariamashraf587@yahoo.com>
2021-01-14 01:15:10 +0200

    Adjusted sidebar and toggle

--- src/components/sidebar.component.js
+++ src/components/sidebar.component.js
@@ -6,9 +6,17 @@ import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Sidebar = (props) => { {
+    const [barStyle, setBarStyle] = useState("show");
+    const [timesStyle, setTimesStyle] = useState("hide");
+    const [toggleWidth, setWidth] = useState("sidebar-closed");
   
     return(
-            <div className="sidebar">
+            <div className="sidebar" id={toggleWidth} onMouseOver={() =>{ setWidth("sidebar-open");}} onMouseLeave={() =>{setBarStyle("show"); 
+                    setTimesStyle("hide"); setWidth("sidebar-closed");}}>
+            <span className="nav-icon text-white toggle"id={barStyle} ><FontAwesomeIcon icon="bars" onClick={() =>{setTimesStyle("show");
+                    setBarStyle("hide"); setWidth("sidebar-open");}} /></span>
+            <span className="nav-icon text-white toggle"id={timesStyle} ><FontAwesomeIcon icon="times" onClick={() =>{setBarStyle("show"); 
+                    setTimesStyle("hide"); setWidth("sidebar-closed");}} /></span>
             <a href="#"><span><FontAwesomeIcon className="sidebar-icon" icon="user"/><span className="icon-text">about</span></span></a><br></br>
             <a href="#"><span><FontAwesomeIcon className="sidebar-icon" icon="user"/><span className="icon-text">about</span></span></a><br></br>
             <a href="#"><span><FontAwesomeIcon className="sidebar-icon" icon="user"/><span className="icon-text">about</span></span></a><br></br>
